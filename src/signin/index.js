955ad17231ed15a30dfa3c04735194423427e6a4
papabarbas <netstereo1@gmail.com>
2016-11-08 18:52:03 -0500

    CAmbiando tìtulo de la pàgina

--- src/signin/index.js
+++ src/signin/index.js
@@ -2,7 +2,7 @@ var page = require('page');
 var template = require('./template');
 
 page('/signin', function(ctx, next){
-  var main = document.getElementById('main-container');
-  main.innerHTML = "";
-  main.appendChild(template);
+  $('title').html('Platizgram - Signin');
+  var main = $('#main-container');
+  main.empty().append(template);
 })
--- src/signup/index.js
+++ src/signup/index.js
@@ -2,7 +2,7 @@ var page = require('page');
 var template = require('./template');
 
 page('/signup', function(ctx, next){
-  var main = document.getElementById('main-container');
-  main.innerHTML = "";
-  main.appendChild(template);
+  $('title').html('Platizgram - Signup');
+  var main = $('#main-container');
+  main.empty().append(template);
 })
