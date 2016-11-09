063bb8682145bb1165425023981f10aeef7e69b3
papabarbas <netstereo1@gmail.com>
2016-11-08 22:18:52 -0500

    Agregando un timeline para los usuarios

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,6 +1,8 @@
 var page = require('page');
+var template = require('./template');
 
 page('/', function(ctx, next){
-  var main = document.getElementById('main-container');
-  main.innerHTML = '<a href="/signup">Signup</a>'
+  $('title').html('Platizgram');
+  var main = $('#main-container');
+  main.empty().append(template);
 })
