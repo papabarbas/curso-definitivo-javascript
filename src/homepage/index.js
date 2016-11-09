bd30f55721b670c32b84afb9703affab215f780f
papabarbas <netstereo1@gmail.com>
2016-11-09 02:38:05 -0500

    Agregando las imágenes de los usuarios al timeline

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -3,6 +3,26 @@ var template = require('./template');
 
 page('/', function(ctx, next){
   $('title').html('Platizgram');
+  var pictures = [
+    {
+      user: {
+        username: 'Hillary-Clinton',
+        avatar: 'http://2016christianvotersguide.com/img/hillary.png'
+      },
+      url: 'office.jpg',
+      likes: 10,
+      liked: true
+    },
+    {
+      user: {
+        username: 'Hillary-Clinton',
+        avatar: 'http://2016christianvotersguide.com/img/hillary.png'
+      },
+      url: 'office.jpg',
+      likes: 2,
+      liked: true
+    },
+  ];
   var main = $('#main-container');
-  main.empty().append(template);
+  main.empty().append(template(pictures));
 })
--- src/homepage/template.js
+++ src/homepage/template.js
@@ -1,10 +1,16 @@
 var yo = require('yo-yo');
 var layout = require('../layout');
+var picture = require('../picture-card');
 
-var template = yo`<div class="container timeline">
-  <div class="row">
-    <div class="col s12 m10 offset-m1 l6 offset-l3">
-    content
-    </div>
-  </div>`;
-module.exports = layout(template);
+module.exports = function(pictures) {
+    var el = yo`<div class="container timeline">
+      <div class="row">
+        <div class="col s12 m10 offset-m1 l6 offset-l3">
+          ${pictures.map(function (pic){
+            return picture(pic);
+          })}
+        </div>
+      </div>
+    </div>`;
+    return layout(el);
+}
